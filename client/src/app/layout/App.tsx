3fa2614239ed9443dd386f4a9dae168122645b58
Yancho <ianchocha@gmail.com>
2023-07-26 15:37:52 +0300

    End Of Section 3

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import { Product } from "../models/product";
+import Catalog from "../../features/catalog/Catalog";
+import { Container, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import Header from "./Header";
+
+function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const palleteType = darkMode ? 'dark' : 'light';
+  const theme = createTheme({
+    palette: {
+      mode: palleteType,
+      background: {
+        default: palleteType === 'light' ? '#eaeaea' : '#121212'
+      }
+    }
+  });
+
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
+      <Container>
+        <Catalog />
+      </Container>
+
+    </ThemeProvider >
+  );
+}
+
+export default App;
